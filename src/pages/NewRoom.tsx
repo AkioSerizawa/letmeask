4f469231a080ea5b0473f76d357aa7bc941073a3
Akio <akio50@hotmail.com>
2021-10-02 11:52:37 -0300

    autenticação

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
@@ -6,8 +8,9 @@ import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
 
-
 export function NewRoom () {
+  const { user } = useContext(AuthContext);
+
     return (
         <div id="page-auth">
       <aside>
@@ -18,6 +21,7 @@ export function NewRoom () {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input 
