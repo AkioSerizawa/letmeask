20f3fd03b25ff434ca6fd757ad6cd3ab89e88354
Akio <akio50@hotmail.com>
2021-10-02 16:09:58 -0300

    Autentificação

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,14 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom'
-import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom () {
-  const { user } = useContext(AuthContext);
+  const { user } = useAuth();
 
     return (
         <div id="page-auth">
